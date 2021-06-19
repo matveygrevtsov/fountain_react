1714b7d8d675b4a118f6e4059e913999b175a9ac
matveygrevtsov <matveigrevtsov@yandex.ru>
2021-06-19 13:21:24 +0300

    Extra fountain removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React, { useEffect, useRef } from 'react'
 import FountainAnimator from './fountain'
 import skin1 from './img/coin1.svg'
 import skin2 from './img/coin2.svg'
-import crown from './img/crown.png'
 
 const App = () => {
   const fountainCanvasRef = useRef<any>(null)
 
   useEffect(() => {
-    const fountainAnimator1 = new FountainAnimator({
+    const fountainAnimator = new FountainAnimator({
       animationSpeed: 2.9,
       power: 155,
       itemsNumber: 52,
@@ -20,24 +19,9 @@ const App = () => {
       imgSkins: [skin1, skin2],
       canvasRef: fountainCanvasRef,
     })
-    fountainAnimator1.start()
-    const fountainAnimator2 = new FountainAnimator({
-      animationSpeed: 2.9,
-      power: 155,
-      itemsNumber: 3,
-      spreadAngle: 99,
-      gravity: 8,
-      minItemsSize: 215,
-      maxItemsSize: 421,
-      rotationSpeed: 0,
-      imgSkins: [crown],
-      canvasRef: fountainCanvasRef,
-    })
-    fountainAnimator2.start()
-    return () => {
-      fountainAnimator1.destroy()
-      fountainAnimator2.destroy()
-    }
+    fountainAnimator.start()
+
+    return () => fountainAnimator.destroy()
   }, [])
 
   return <canvas id="fountain1" ref={fountainCanvasRef} />
